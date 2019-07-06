7b7c019717f1dcfec0fc3e7f38f3ebc3a81f3e5f
George <net.george.sky@gmail.com>
2019-07-06 16:13:40 -0700

    add application helper

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,8 @@
+# require 'test_helper'
+#
+# class ApplicationHelper < ActionView::TestCase
+#   test "full title helper" do
+#     assert_select full_title,
+#     assert_equal full_title("Help"),
+#   end
+# end
