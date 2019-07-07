2c8a5e2f959ae5b02f543b20a3aafb9fdfe7207a
George <net.george.sky@gmail.com>
2019-07-07 15:07:12 -0700

    commented out test/fixtures/line 2

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,37 +11,38 @@ class UserTest < ActiveSupport::TestCase
     assert @user.valid?
   end
 
-  # test "name should be present" do
-  #   @user.name = "     "
-  #   assert_not @user.valid?
-  # end
-  #
-  # test "email should be present" do
-  #   @user.email = "     "
-  #   assert_not @user.valid?
-  # end
-  #
-  # test "email should not exceed 255 characters" do
-  #   @user.email = "@" * 256
-  #   assert_not @user.valid?
-  # end
-  #
-  # test "name should not exceed 30 characters" do
-  #   @user.name = "@" * 31
-  #   assert_not @user.valid?
-  # end
-  #
-  # test "email validation" do
-  #   addresses = %w[user@ser+ver.com user+name@@server.com]
-  #   addresses.each do |email|
-  #     @user.email = email
-  #     assert_not @user.valid?, "#{email.inspect} should be invalid"
-  # end
-  #
-  # test "emails are unique" do
-  #   duplicate_user = @user.dup
-  #   duplicate_user.email = @user.email.upcase
-  #   @user.save
-  #   assert_not duplicate_user.valid?
-  # end
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
+  test "email should not exceed 255 characters" do
+    @user.email = "@" * 256
+    assert_not @user.valid?
+  end
+
+  test "name should not exceed 30 characters" do
+    @user.name = "@" * 31
+    assert_not @user.valid?
+  end
+
+  test "email validation" do
+    addresses = %w[user@ser+ver.com user+name@@server.com]
+    addresses.each do |email|
+      @user.email = email
+      assert_not @user.valid?, "#{email.inspect} should be invalid"
+    end
+  end
+
+  test "emails are unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
