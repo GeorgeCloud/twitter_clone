dd3564a76997276ca68102eeaefd35993d90e764
George <net.george.sky@gmail.com>
2019-07-08 10:30:07 -0700

    add flash box

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  # Invalid Password Information
+  # Validates Account is Created
+  test "successful account creation" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "testName",
+                                         email: "test@email.com",
+                                         password: "testPassword",
+                                         password_confirmation: "testPassword"
+
+      }}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    # assert_not flash.danger
+  end
+
+  # Validates Account is not created with Invalid Data
   test "invalid user signup" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -9,7 +25,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          email: "newUser@gmail.com",
                                          password: "password",
                                          password_confirmation: "foo"
-                                         }}
+      }}
     end
     assert_template 'users/new'
   end
