23bce075eaaa9016cc14312d300cc8ed0408a071
George <net.george.sky@gmail.com>
2019-07-08 13:31:07 -0700

    add log_in helper & render box for incorrect login attempt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # User log in and redirect_to
+      log_in(user)
+      redirect_to user_url(user)
+    else
+      # Prompt User error_message
+      flash.now[:danger] = "hello world"
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,13 @@ Rails.application.routes.draw do
   get 'sessions/new'
   #Routes '/' to Home Page
   root 'static_pages#home'
+  get '/help',    to: 'static_pages#help'
+  get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-  get '/home', to: 'static_pages#home'
-  get '/help', to: 'static_pages#help'
-  get '/about', to: 'static_pages#about'
-  get '/login', to: 'static_pages#login'
+  get '/signup',  to: 'users#new'
+  get '/login',   to: 'sessions#new'
+  post '/login',  to: 'sessions#create'
 
-  # User Routes
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
